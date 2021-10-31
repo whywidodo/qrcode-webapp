6c843e0c2ea491f04ade78c5afbbd68fe4ca8d1d
whywidodo <mr.wahyuwidodo@gmail.com>
2021-10-31 22:01:28 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import qrcode
+
+
+st.set_page_config(
+    page_title="Konversi Link - QrCode",
+    initial_sidebar_state="expanded",
+)
+
+link = st.text_input("Masukkan link website : ")
+
+img = qrcode.make(link)
+img.save("Link2.jpg")
