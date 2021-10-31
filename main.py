fe2f953e83737d41ac71cd200b8c772fb85b16b5
whywidodo <mr.wahyuwidodo@gmail.com>
2021-10-31 22:26:01 +0700

    update file

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import qrcode
 
+st.title('QRCode Generator')
 
-st.set_page_config(
-    page_title="Konversi Link - QrCode",
-    initial_sidebar_state="expanded",
-)
+hyper_link = st.text_input("Tuliskan link :",
+                           "https://www.google.com")
 
+qr_size = st.slider('Perbesar ', min_value=6, max_value=12, value=6)
+st.write("Ukuran", qr_size)
 
-link = st.text_input("Masukkan link website : ")
-img = qrcode.make(link)
-# img.save("Link2.jpg")
-st.image(img, caption='Link From')
+# (3.1) without logo
+qr_name = qrcode.qr_code(link=hyper_link, logo=False, size=qr_size)
+st.image(qr_name, caption='Gambar QRCode')
